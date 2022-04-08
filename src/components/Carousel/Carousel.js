8007344b89ee8e8899840cb29f6a33cb8ff3d276
Adam Fałek <adm.falek@gmail.com>
2022-04-08 07:45:10 +0200

    [GA-25] - code structure fixes

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -5,13 +5,12 @@ import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons"
 import { positionCharacters, goLeft, goRight } from "../../utils/Utils"
 import CarouselItem from "./CarouselItem"
 
-const Carousel = ( {characters}) => {
-
+const Carousel = ({characters}) => {
   const charactersContainer= useRef(null) 
   const [currentIndex, setCurrentIndex] = useState(1);
 
   useEffect(() => {
-    if(characters.length > 0) {
+    if (characters.length > 0) {
       const all = charactersContainer.current.children
       const runner = all[0]
       const builder = all[1]
@@ -23,34 +22,36 @@ const Carousel = ( {characters}) => {
   return (
     <article className="carousel">
       <FontAwesomeIcon 
-          icon={faArrowLeft} 
-          size="5x" 
-          onClick={()=>goLeft(setCurrentIndex, currentIndex)} 
+        icon={faArrowLeft} 
+        size="5x" 
+        onClick={()=>goLeft(setCurrentIndex, currentIndex)} 
       />
-        <div className="items-carousel">
-          {characters.length > 0 ? (
-            <div className="flex-justify-around" ref={charactersContainer}>
-              {characters.map( (character,index) => {
-                const name  = character.name
-                const description = character.description
-                const img = character.img
-                return(
-                  <CarouselItem 
-                    name={name} 
-                    description={description} 
-                    img={img} 
-                    key={index}/>
-                )
-              })}
-            </div>
-          ) : null}
-        </div>
-        <FontAwesomeIcon 
-            icon={faArrowRight} 
-            size="5x" 
-            onClick={()=>goRight(setCurrentIndex, currentIndex)} 
-        />
+      <div className="items-carousel">
+        {characters.length > 0 ? (
+          <div className="flex-justify-around" ref={charactersContainer}>
+            {characters.map( (character,index) => {
+              const name  = character.name
+              const description = character.description
+              const img = character.img
+              return (
+                <CarouselItem 
+                  name={name} 
+                  description={description} 
+                  img={img} 
+                  key={index}
+                />
+              )
+            })}
+          </div>
+        ) : null}
+      </div>
+      <FontAwesomeIcon 
+        icon={faArrowRight} 
+        size="5x" 
+        onClick={()=>goRight(setCurrentIndex, currentIndex)} 
+      />
     </article>
   );
 };
+
 export default Carousel;
